17a15c36c01b1822b150e68f5e2c244bd616d79b
Aliaksei Salavei <aliakseisalavei.contact@gmail.com>
2024-06-10 13:56:43 +0400

    refactor: add Singleton with Early Initialization `Vocabulary INSTANCE`

--- src/main/java/com/asalavei/hangman/Vocabulary.java
+++ src/main/java/com/asalavei/hangman/Vocabulary.java
@@ -8,44 +8,39 @@ import java.util.Scanner;
 
 public class Vocabulary {
 
-    private List<String> vocabularyList;
-    private Random random;
+    private final List<String> vocabularyList;
+    private final Random random;
     private String word;
 
+    private static Vocabulary INSTANCE = new Vocabulary();
 
-    public Vocabulary() {
-        this(new Random());
-    }
-
-    public Vocabulary(Random random) {
+    private Vocabulary() {
         this.vocabularyList = loadVocabulary();
-        this.random = random;
-        this.word = getRandomWord();
+        this.random = new Random();
     }
 
     public String getWord() {
         return word;
     }
 
-    public List<String> loadVocabulary() {
-        vocabularyList = new ArrayList<>();
+    public static Vocabulary getInstance() {
+        return INSTANCE;
+    }
+
+    private List<String> loadVocabulary() {
+        List<String> list = new ArrayList<>();
         try (InputStream inputStream = Vocabulary.class.getResourceAsStream("/vocabulary.txt");
              Scanner scanner = new Scanner(inputStream)) {
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                vocabularyList.add(line);
+                list.add(line);
             }
 
         } catch (Exception e) {
             throw new RuntimeException("Error reading vocabulary file.", e);
         }
 
-        return vocabularyList;
-    }
-
-    public String getRandomWord() {
-        int index = random.nextInt(vocabularyList.size());
-        return vocabularyList.get(index);
+        return list;
     }
 }
