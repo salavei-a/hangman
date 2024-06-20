75c20d5202090f52f2e0b76164e67f1fcacee258
Aliaksei Salavei <aliakseisalavei.contact@gmail.com>
2024-06-20 12:50:49 +0400

    refactor: `createGame()`

--- src/main/java/com/asalavei/hangman/GameFactory.java
+++ src/main/java/com/asalavei/hangman/GameFactory.java
@@ -1,11 +1,18 @@
 package com.asalavei.hangman;
 
+import com.asalavei.hangman.vocabulary.EnglishVocabularyFactory;
+import com.asalavei.hangman.vocabulary.RussianVocabularyFactory;
 import com.asalavei.hangman.vocabulary.VocabularyLanguage;
 import com.asalavei.hangman.vocabulary.VocabularyFactory;
 
 public class GameFactory {
 
-    public Game createGame(VocabularyFactory vocabularyFactory, VocabularyLanguage vocabularyLanguage) {
+    public Game createGame(VocabularyLanguage vocabularyLanguage) {
+        VocabularyFactory vocabularyFactory = switch (vocabularyLanguage) {
+            case RUSSIAN -> new RussianVocabularyFactory();
+            case ENGLISH -> new EnglishVocabularyFactory();
+        };
+
         return new Game(vocabularyFactory, new Hangman(), vocabularyLanguage);
     }
 }
--- src/main/java/com/asalavei/hangman/Main.java
+++ src/main/java/com/asalavei/hangman/Main.java
@@ -1,24 +1,10 @@
 package com.asalavei.hangman;
 
-import com.asalavei.hangman.vocabulary.*;
+import com.asalavei.hangman.vocabulary.VocabularyLanguage;
 
 public class Main {
     public static void main(String[] args) {
-        VocabularyFactory vocabularyFactory;
-        VocabularyLanguage vocabularyLanguage;
-
-        String selectedVocabularyLanguage = "russian";
-
-        if ("russian".equals(selectedVocabularyLanguage)) {
-            vocabularyFactory = new RussianVocabularyFactory();
-            vocabularyLanguage = VocabularyLanguage.RUSSIAN;
-        } else {
-            vocabularyFactory = new EnglishVocabularyFactory();
-            vocabularyLanguage = VocabularyLanguage.ENGLISH;
-        }
-
-        GameFactory gameFactory = new GameFactory();
-        Game game = gameFactory.createGame(vocabularyFactory, vocabularyLanguage);
+        Game game = new GameFactory().createGame(VocabularyLanguage.RUSSIAN);
         game.startGame();
     }
 }
